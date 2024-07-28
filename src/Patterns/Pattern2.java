bd85ca8624a78e739b3146248a8c48f4de5f6663
Tridib11 <tridibghosh12345@gmail.com>
2024-07-29 01:19:39 +0530

    Files added

--- src/Patterns/Pattern2.java
+++ src/Patterns/Pattern2.java
@@ -6,7 +6,7 @@ public class Pattern2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int totalStar=1;
+        int totalStars=1;
         int totalRows=n;
         int totalSpaces=n-1;
         for(int row=1;row<=totalRows;row++){
@@ -15,11 +15,11 @@ public class Pattern2 {
                 System.out.print(" ");
             }
             //stars
-            for(int star=1;star<=totalStar;star++){
+            for(int star=1;star<=totalStars;star++){
                 System.out.print("*");
             }
             System.out.println();
-            totalStar+=2;
+            totalStars+=2;
             totalSpaces-=1;
         }
     }
