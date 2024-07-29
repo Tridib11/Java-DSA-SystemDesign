36301929997ce846e1c0ae4662895a9ac45a30ab
Tridib11 <tridibghosh12345@gmail.com>
2024-07-29 22:14:47 +0530

    Files added

---
+++ src/Patterns/Pattern3.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int totalRows=sc.nextInt();
+        int totalStars=1;
+        for(int rows=1;rows<=totalRows;rows++){
+            for(int star=1;star<=totalStars;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+            if(rows<=(totalRows/2)){
+                totalStars++;
+            }else{
+                totalStars--;
+            }
+        }
+    }
+}
