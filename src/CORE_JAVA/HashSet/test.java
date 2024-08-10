12f730db293f184e5fb342b7fbe8d58704dcf575
Tridib11 <tridibghosh12345@gmail.com>
2024-08-10 17:54:02 +0530

    Files added

--- src/CORE_JAVA/HashSet/test.java
+++ src/CORE_JAVA/HashSet/test.java
@@ -2,7 +2,7 @@ package CORE_JAVA.HashSet;
 import java.util.*;
 public class test {
     public static void main(String[] args) {
-        HashSet<Integer> set=new HashSet<>();
+        HashSet<Integer> set=new HashSet<>(); //usnique elements
         //Insert
         set.add(1);
         set.add(2);
@@ -15,6 +15,18 @@ public class test {
         }else{
             System.out.println("false");
         }
+
+        //delete
+        set.remove(1);
+        if(set.contains(1)){
+            System.out.println("True");
+        }else{
+            System.out.println("false");
+        }
+
+        //Length
+
+        System.out.println("THe size of the set is "+set.size());
 
     }
 }
