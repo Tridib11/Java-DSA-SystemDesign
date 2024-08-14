8656245d687259c6d149ee53b9202f442ee1601b
Tridib11 <tridibghosh12345@gmail.com>
2024-08-15 01:53:08 +0530

    Files added

---
+++ src/MaximumConsicutiveOnes.java
@@ -0,0 +1,19 @@
+import java.util.* ;
+import java.io.*;
+public class MaximumConsicutiveOnes {
+    public static int consecutiveOnes(int n, int[] arr) {
+        // Write your code here.
+        int maxOnes=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==1){
+                count+=1;
+            }else{
+                count=0;
+            }
+            maxOnes=Math.max(maxOnes,count);
+        }
+
+        return maxOnes;
+    }
+}
