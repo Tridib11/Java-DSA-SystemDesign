d272e6c5de521f94a9aa3dff806bbbbaf0f23842
Tridib11 <tridibghosh12345@gmail.com>
2024-08-15 01:38:02 +0530

    Files added

---
+++ src/MissingNumber.java
@@ -0,0 +1,13 @@
+public class MissingNumber {
+    public static int missingNumber(int []a, int N) {
+        // Write your code here.
+        int sum=0,actualSum=0;
+        for(int i=0;i<a.length;i++){
+            sum+=a[i];
+        }
+
+        actualSum=(N*(N+1))/2;
+        int missingNumber=actualSum-sum;
+        return missingNumber;
+    }
+}
