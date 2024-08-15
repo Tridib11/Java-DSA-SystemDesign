40deb67df9ed10065ca85af3927f05b7222d4363
Tridib11 <tridibghosh12345@gmail.com>
2024-08-15 22:40:54 +0530

    Files added

--- src/MaximumConsicutiveOnes.java
+++ src/MaximumConsicutiveOnes.java
@@ -6,6 +6,9 @@ public class MaximumConsicutiveOnes {
         int maxOnes=0;
         int count=0;
         for(int i=0;i<n;i++){
+
+
+
             if(arr[i]==1){
                 count+=1;
             }else{
