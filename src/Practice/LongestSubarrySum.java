f469faa56e29b2ab442780781bb66b78c08f4346
Tridib11 <tridibghosh12345@gmail.com>
2024-08-27 03:20:20 +0530

    Files added

--- src/Practice/LongestSubarrySum.java
+++ src/Practice/LongestSubarrySum.java
@@ -8,8 +8,19 @@ public class LongestSubarrySum {
     public static int getLongestSubarray(int []nums, int k) {
         // Write your code here.
         int largestLength=0;
+        int start=0,end=0;
+        int currentSum=0;
         HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            currentSum+=nums[i];
+            if(currentSum-k==0){
+                start=0;
+                end=i;
+                largestLength=Math.max(largestLength,(end-start)+1);
+            }else{
 
+            }
+        }
 
     }
 }
