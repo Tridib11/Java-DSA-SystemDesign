dcf210757c001add083fd9dcef2932ca1b036ff0
Tridib11 <tridibghosh12345@gmail.com>
2024-08-26 23:23:49 +0530

    Files added

--- src/Practice/MergeSortedArray.java
+++ src/Practice/MergeSortedArray.java
@@ -2,8 +2,29 @@ package Practice;
 
 import java.util.*;
 public class MergeSortedArray {
+
+    public static void main(String[] args) {
+
+    }
     public static List< Integer > sortedArray(int []a, int []b) {
         // Write your code here
+        int i=0,j=0;
+        List<Integer> ans=new ArrayList<>();
+        while(i<a.length && j<b.length ){
+            if(a[i]<=b[j]){
+                if(ans.isEmpty() || ans.getLast()!=a[i]){
+                    ans.add(a[i]);
+                }
+                i++;
+            }
+            else{
+                if(ans.isEmpty() || ans.getLast()!=b[j]){
+                    ans.add(b[j]);
+                }
+                j++;
+            }
+        }
+
 
 
 
