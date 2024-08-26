ef9768dc41bba040f2c3ad3e7f1a8452d4ce3c25
Tridib11 <tridibghosh12345@gmail.com>
2024-08-27 03:13:02 +0530

    Files added

--- src/Practice/LongestSubarrySum.java
+++ src/Practice/LongestSubarrySum.java
@@ -1,7 +1,15 @@
 package Practice;
-
+import java.util.*;
 public class LongestSubarrySum {
     public static void main(String[] args) {
+
+    }
+
+    public static int getLongestSubarray(int []nums, int k) {
+        // Write your code here.
+        int largestLength=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+
 
     }
 }
